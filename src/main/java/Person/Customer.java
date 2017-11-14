c618067ca6e1835b0353dea064d59e57c3fedf1a
Ciaran-OBrien <c15765215@mydit.ie>
2017-11-14 12:37:57 +0100

    Adding first iteration of Person interface with a customer class

---
+++ src/main/java/Person/Customer.java
@@ -0,0 +1,33 @@
+/*
+ * Authors: Ciarán O'Brien
+ * Lecture: Bennard Humm
+ */
+package Person;
+
+/**
+ *
+ * @authors Ciaran
+ */
+public class Customer implements IPerson{
+
+    @Override
+    public void setName() {
+
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public void setAddres() {
+
+    }
+
+    @Override
+    public String getAddress() {
+        return null;
+    }
+    
+}
---
+++ src/main/java/Person/IPerson.java
@@ -0,0 +1,20 @@
+/*
+ * Authors: Ciarán O'Brien
+ * Lecture: Bennard Humm
+ */
+package Person;
+
+/**
+ *
+ * @authors Ciaran
+ */
+public interface IPerson {
+    
+    public void setName();
+    
+    public String getName();
+
+    public void setAddres();
+    
+    public String getAddress();
+}
